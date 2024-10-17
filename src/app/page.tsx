6fcebd05d5ee88d38827ddd71e9a18cb255ba02d
edvxndxvi <edvandavi1@hotmail.com>
2024-10-17 17:56:04 -0300

    Adicionando cores padrões no Tailwind CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function PageHome() {
   return (
     <main>
-      <h1>Home</h1>    
+      <h1>Home</h1> 
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,16 @@ const config: Config = {
   content: [ "./src/**/*.{js,ts,jsx,tsx,mdx}" ],
   theme: {
     extend: {
+      colors:{
+        preto: 'var(--preto)',
+        cinzaEscuro: 'var(--cinza-escuro)',
+        cinzaClaro: 'var(--cinza-claro)',
+        branco: 'var(--branco)',
+        corP1: 'var(--cor-p1)',
+        corP2: 'var(--cor-p2)',
+        corP3: 'var(--cor-p3)',
+        corP4: 'var(--cor-p4)',
+      }
     },
   },
   plugins: [],
